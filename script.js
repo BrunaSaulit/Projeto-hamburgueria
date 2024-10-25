965523fa8a532c5c3c8fc6820e0dd696231e8ed4
BrunaSaulit <saulitbruna@gmail.com>
2024-10-24 22:12:04 -0300

    estou criando a sessão oferta

---
+++ script.js
@@ -0,0 +1,12 @@
+//INICIO CÓDIGO INDEX
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
+//FIM CÓDIGO INDEX
+//INICIO CÓDIGO SEÇÃO HAMBURGUER
+
+//FIM CÓDIGO SEÇÃO HAMBURGUER
